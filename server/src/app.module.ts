80178e7d299488b818c1a13e387a5591e3e23779
wiss <yousfiwissem224@gmail.com>
2023-05-31 11:31:14 +0100

    entities

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,20 +1,30 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
-
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { StudentModule } from './student/student.module';
+import { MenuModule } from './menu/menu.module';
+import { VoteModule } from './vote/vote.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3308,
-      username: 'root',
-      password: '',
-      database: 'tpnest',
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "webproject",
       entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true,
-    })
+    }),
+    RestaurantModule,
+    StudentModule,
+    MenuModule,
+    VoteModule
   ],
   controllers: [AppController],
   providers: [AppService],
