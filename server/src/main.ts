4eaaf746208705f12cf493fa93ba4f517c7222fa
MehdiBenSalha <mehdi.bensalha@insat.u-carthage.tn>
2023-05-31 10:55:25 +0100

    env

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import * as dotenv from 'dotenv';
+import {ConfigService} from "@nestjs/config";
 
 dotenv.config();
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.APP_PORT);
