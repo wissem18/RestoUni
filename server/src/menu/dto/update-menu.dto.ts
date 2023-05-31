36f449783299dad15052359bd5ed29d11b2e7087
MehdiBenSalha <mehdi.bensalha@insat.u-carthage.tn>
2023-05-31 12:14:27 +0100

    menu

--- server/src/menu/dto/update-menu.dto.ts
+++ server/src/menu/dto/update-menu.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMenuDto } from './create-menu.dto';
+import {IsNotEmpty, IsNumber, IsString} from "class-validator";
 
-export class UpdateMenuDto extends PartialType(CreateMenuDto) {}
+export class UpdateMenuDto extends PartialType(CreateMenuDto) {
+    @IsNotEmpty()
+    @IsString()
+    entrée: string;
+
+    @IsNotEmpty()
+    @IsString()
+    plat: string;
+    @IsNotEmpty()
+    @IsString()
+    dessert: string;
+    @IsNotEmpty()
+    @IsNumber()
+    restaurantId: number;
+}
--- server/src/menu/entities/menu.entity.ts
+++ server/src/menu/entities/menu.entity.ts
@@ -1,1 +1,23 @@
-export class Menu {}
+import {Column, CreateDateColumn, DeleteDateColumn, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {Entity, PrimaryColumn} from "typeorm";
+import {Restaurant} from "../../restaurant/entities/restaurant.entity";
+@Entity('menu')
+export class Menu {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    entrée: string;
+    @Column()
+    plat: string;
+    @Column()
+    dessert: string;
+    @CreateDateColumn()
+    createdAt: Date;
+    @DeleteDateColumn()
+    deletedAt: Date;
+
+    @ManyToOne(() => Restaurant, Restaurant => Restaurant.id)
+    restaurant: Restaurant;
+
+
+}
