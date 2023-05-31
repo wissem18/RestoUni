7280244aee9f1b0e04f8c425ba9b92004216d59c
wiss <yousfiwissem224@gmail.com>
2023-05-31 13:33:44 +0100

    timestamp

--- server/src/student/entities/student.entity.ts
+++ server/src/student/entities/student.entity.ts
@@ -3,13 +3,12 @@ import { Col } from 'react-bootstrap';
 import { Restaurant } from 'src/restaurant/entities/restaurant.entity';
 import { TimeStampEntity } from 'src/timestamp/timpestamp.entity';
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
-
-
+import {v4 as uuid} from 'uuid';
 @Entity()
 export class Student extends TimeStampEntity{
   @PrimaryGeneratedColumn()
-  id: number;
-
+  id: uuid;
+  
   @Column({ length: 50 })
   firstname: string;
 
