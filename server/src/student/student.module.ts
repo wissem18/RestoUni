7b44b43eb23f5b4383b65ee2601eb43469a0deb8
amine <mohamedamineguesmi@insat.u-carthage.tn>
2023-05-31 15:23:26 +0100

    student service

--- server/src/student/student.module.ts
+++ server/src/student/student.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { Student } from './entities/student.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [StudentController],
-  providers: [StudentService]
+  providers: [StudentService],
+  imports: [TypeOrmModule.forFeature([Student])],
+
 })
 export class StudentModule {}
