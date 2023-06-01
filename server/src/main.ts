6c74330a1e1cc1c1b67781cd45e29fcb63e1fb1b
amine <mohamedamineguesmi@insat.u-carthage.tn>
2023-06-01 10:01:38 +0100

    user login/signup

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import * as cors from 'cors';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,7 +10,8 @@ dotenv.config();
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  await app.listen(3000);
+  await app.listen(3006);
 }
 bootstrap();
